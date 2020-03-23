9163f1ed537196113cedf7bae4aea7adccbbd337
LGsus Rmz <caednicolas2@gmail.com>
2020-03-23 17:08:10 -0600

    Edit Menu

---
+++ Fetch/js/Practica1.js
@@ -0,0 +1,22 @@
+var paises = document.getElementById("paises");
+
+fetch('datos.json')
+.then(function(response) {
+
+    response.json().then(function(datos) {
+
+        datos.forEach( registro => {
+            
+            console.log(registro);
+            let nombre = document.createElement("p");
+            nombre.textContent = "El País: " + registro.country + ", tiene: " + registro.cases+" de casos";
+            paises.appendChild(nombre);
+
+        });
+
+    });
+
+})
+.catch(function(error) {
+    console.log('Hubo un problema con la petición Fetch:' + error.message);
+});
