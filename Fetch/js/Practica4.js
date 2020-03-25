f45c9548ecfde259ecaaee6e18459b99b4c2544e
LGsus Rmz <caednicolas2@gmail.com>
2020-03-24 18:30:37 -0600

    Finish v4

--- Fetch/js/Practica4.js
+++ Fetch/js/Practica4.js
@@ -20,7 +20,7 @@ function iniciaMapa() {
 
             const map = new google.maps.Map(document.getElementById('map'), propiedades);
 
-            console.log(datos);
+            //console.log(datos);
 
             datos.forEach( marcador => {
 
@@ -43,7 +43,7 @@ function iniciaMapa() {
                                 });
                                 var icono={
                                     url:"https://cdn3.iconfinder.com/data/icons/map-markers-1/512/biohazard-512.png",
-                                    scaledSize: new google.maps.Size(20,40),
+                                    scaledSize: new google.maps.Size(40,40),
                                     origin: new google.maps.Point(0,0),
                                     anchor: new google.maps.Point(0,0),
                                 };
