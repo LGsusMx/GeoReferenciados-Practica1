3fc1644277ae48d169d403d6c2113339d25f7828
LGsus Rmz <caednicolas2@gmail.com>
2020-06-11 14:25:23 -0500

    FixFirebaseConfig

---
+++ ProyectoFinal/firebasecon.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCuyaG1zPv63VICawS7TA71cd6ndVf9vK8",
+    authDomain: "autorizacion-67410.firebaseapp.com",
+    databaseURL: "https://autorizacion-67410.firebaseio.com",
+    projectId: "autorizacion-67410",
+    storageBucket: "autorizacion-67410.appspot.com",
+    messagingSenderId: "111149213958",
+    appId: "1:111149213958:web:30259361cbb3fcef8f5687"
+
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const db = firebase.firestore();
