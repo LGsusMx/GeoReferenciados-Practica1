8daa43aabb76c344d21d161195bdf59721002662
LGsus Rmz <caednicolas2@gmail.com>
2020-06-11 01:47:28 -0500

    Update mapOrigin.js

--- ProyectoFinal/mapOrigin.js
+++ ProyectoFinal/mapOrigin.js
@@ -1,11 +1,12 @@
 
 var script= null;
-var scriptOriginal;
+var scriptOriginal = document.createElement('scriptOriginal');
 scriptOriginal.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDtdoQeXdlgm1q7E8gEVrxZeOA-8fssxOM&libraries=places&callback=iniciaMapa';
 document.head.appendChild(scriptOriginal);
 function editarLenguaje(idioma){
     if(scriptOriginal == null){
         document.head.removeChild(scriptOriginal);
+        script = document.createElement('script');
     }
     if (script !=null) {
         document.head.removeChild(script);
